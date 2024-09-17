d60dbe43dc1d0638acd5d29a01f889710346d278
mitsunaga <sauimff27@yahoo.co.jp>
2024-09-17 21:00:09 +0900

    WIP:地下画面の作成(次の画面に切り替える部分は未完成)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Top from "./_components/Top";
+import UnderGround from "./_components/UnderGround";
 
 const page = () => {
   return (
-    <div>
-      <Top />
-      <div className="h-screen">aaa</div>
+    <div className="relative">
+      <div className="sticky top-0 z-20">
+        <Top />
+      </div>
+      <UnderGround />
     </div>
   );
 };
