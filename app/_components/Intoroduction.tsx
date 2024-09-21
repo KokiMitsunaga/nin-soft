4b58034eb64bc59e33e5a717b061b046a6fe2c82
mitsunaga <sauimff27@yahoo.co.jp>
2024-09-22 01:14:04 +0900

    pc版のデザインを変更

--- app/_components/Intoroduction.tsx
+++ app/_components/Intoroduction.tsx
@@ -4,9 +4,8 @@ import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Nunito_Sans } from "next/font/google";
-import ListButton from "./ListButton";
-import GenreSelectBox from "./GenreSelectBox";
 import gamesData from "../data/gamesData.json";
+import Sidebar from "./Sidebar";
 
 // ゲーム情報の型定義
 interface GameInfo {
@@ -67,10 +66,10 @@ const Intoroduction = () => {
   useEffect(() => {
     const handleScroll = (event: WheelEvent) => {
       const scrollContainer = scrollContainerRef.current;
-      const intoroductioin = intoroductionRef.current;
+      const intoroduction = intoroductionRef.current;
 
-      if (scrollContainer && intoroductioin) {
-        const rect = intoroductioin.getBoundingClientRect();
+      if (scrollContainer && intoroduction) {
+        const rect = intoroduction.getBoundingClientRect();
         const isIntoroductionInView =
           rect.top <= 0 && rect.bottom >= window.innerHeight;
 
@@ -78,57 +77,36 @@ const Intoroduction = () => {
           // 横スクロール操作
           scrollContainer.scrollLeft += event.deltaY;
 
-          // スクロール方向に応じて背景位置を進める・戻す
-          const scrollFactor = 0.2; // 背景が進むスピード
-          setBackgroundPosition(
-            (prevPosition) => prevPosition - event.deltaY * scrollFactor
-          );
-
           const contentWidth = scrollContainer.clientWidth / 1.3;
           const scrollLeft = scrollContainer.scrollLeft;
+          const maxScrollLeft =
+            scrollContainer.scrollWidth - scrollContainer.clientWidth;
 
           // スクロール位置に基づいて表示するコメントを更新
-          const visibleIndex = Math.min(
-            Math.round(scrollLeft / contentWidth), // 変更: Math.floor から Math.round へ
-            gameInfo.length - 1
-          );
+          let visibleIndex = Math.round(scrollLeft / contentWidth);
+
+          // 正方向のスクロール時、最後のコンテンツで正確にコメントを表示
+          if (event.deltaY > 0) {
+            visibleIndex = Math.min(visibleIndex, gameInfo.length - 1);
+          } else {
+            // 逆方向のスクロール時、インデックスが負の数になるのを防ぐ
+            visibleIndex = Math.max(visibleIndex, 0);
+          }
 
           // コメントの更新
           setComment(commentList[visibleIndex]);
 
+          // 背景画像のスクロール操作
+          if (scrollLeft < maxScrollLeft && scrollLeft > 0) {
+            const scrollFactor = 1; // 背景が進むスピード
+            setBackgroundPosition(-(scrollLeft * scrollFactor));
+          }
+
           // スクロールがコンテンツの終わりに到達した場合
-          if (
-            scrollLeft + scrollContainer.clientWidth >=
-            scrollContainer.scrollWidth
-          ) {
+          if (scrollLeft >= maxScrollLeft) {
             setScrollCompleted(true);
           } else if (scrollLeft === 0) {
             setScrollCompleted(false);
-          }
-
-          // スクロールがコンテンツの最初または最後で止まる処理
-          if (
-            (scrollLeft === 0 && event.deltaY < 0 && !scrollCompleted) ||
-            (scrollLeft + scrollContainer.clientWidth ===
-              scrollContainer.scrollWidth &&
-              event.deltaY > 0 &&
-              scrollCompleted)
-          ) {
-            return;
-          }
-
-          // コンテンツが最初や最後に達したときに背景の動きを止める
-          if (
-            scrollLeft === 0 ||
-            scrollLeft + scrollContainer.clientWidth ===
-              scrollContainer.scrollWidth
-          ) {
-            return;
-          }
-
-          // コンテンツが途中の場合は背景の動きを継続
-          if (scrollLeft > 0 && event.deltaY < 0) {
-            event.preventDefault();
           }
         }
       }
@@ -155,82 +133,95 @@ const Intoroduction = () => {
   }, [scrollCompleted, gameInfo, commentList]);
 
   return (
-    <div ref={intoroductionRef} className="relative w-full h-screen z-20">
-      <div
-        className="absolute inset-0 z-10"
-        style={{
-          backgroundImage: `url('${bgImage}')`,
-          backgroundPosition: `${backgroundPosition}px 0`,
-          backgroundSize: "cover",
-          transition: "background-position 0.1s linear",
-        }}
-      />
-
-      {/* セレクトボックス */}
-      <GenreSelectBox onSelectChange={handleSelectChange} />
-
-      {/* ボタン */}
-      <ListButton />
-
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center z-30">
-        {/* 横スクロールコンテナ */}
+    <div ref={intoroductionRef} className="relative h-screen z-20 flex">
+      {/* Main content */}
+      <div className="w-5/6 h-full relative">
         <div
-          ref={scrollContainerRef}
-          className="flex overflow-x-auto whitespace-nowrap w-full px-8 space-x-4 scroll-container"
-        >
-          {gameInfo.map((game) => (
-            <Link href={`${game.url}`} key={game.id}>
-              <div
-                className="inline-block bg-white p-4 rounded-lg flex-shrink-0 link-container"
-                style={{
-                  width: "calc(100vw / 1.3)",
-                  height: "calc(100vh - 70vh)",
-                }}
-              >
-                <div className="w-full h-auto mb-4">
+          className="absolute inset-0 z-10"
+          style={{
+            backgroundImage: `url('${bgImage}')`,
+            backgroundPosition: `${backgroundPosition}px 0`,
+            backgroundSize: "cover",
+            transition: "background-position 0.1s linear",
+          }}
+        />
+
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-start z-30">
+          {/* 横スクロールコンテナ */}
+          <div
+            ref={scrollContainerRef}
+            className="flex overflow-x-hidden whitespace-nowrap w-full px-8 space-x-96 scroll-container"
+            style={{ paddingLeft: "16vw", paddingRight: "16vw" }} // 左に少し余白を調整
+          >
+            {gameInfo.map((game) => (
+              <Link href={`${game.url}`} key={game.id}>
+                <div
+                  className="inline-block flex-shrink-0 link-container overflow-hidden relative group bg-white p-10 rounded-3xl" // 背景白、パディング追加
+                  style={{
+                    width: "calc(100vw / 2)",
+                    marginTop: "calc(100vh - 95vh)",
+                  }}
+                >
                   <Image
                     src={game.image}
                     alt={game.alt}
                     layout="responsive"
                     width={900}
                     height={600}
-                    className="object-cover rounded-lg"
+                    className="object-cover rounded-3xl w-full h-full"
                   />
+                  {/* タイトルと説明を重ねて表示 */}
+                  <div className="link-content absolute bottom-0 left-0 w-full p-20 bg-black bg-opacity-60 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-lg">
+                    <h3 className="font-bold text-3xl">{game.alt}</h3>
+                    <p className="text-xl mt-2">{game.description}</p>
+                  </div>
                 </div>
-                <div className="text-center link-content">
-                  <h3 className="font-bold text-lg">{game.alt}</h3>
-                  <p className="text-sm mt-2">{game.description}</p>
-                </div>
+              </Link>
+            ))}
+          </div>
+          <div
+            className="flex items-center"
+            style={{
+              marginTop: "calc(100vh - 98vh)",
+              marginLeft: "calc(100vw - 94vw)",
+            }}
+          >
+            {/* 人物画像 */}
+            <div className="z-30 flex justify-center items-center">
+              <div className="relative w-72 h-72 overflow-hidden rounded-full">
+                <Image
+                  alt="human"
+                  src={humanImage}
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-full"
+                />
               </div>
-            </Link>
-          ))}
-        </div>
-
-        {/* コメント表示エリア */}
-        <div className="absolute pt-20 px-8 flex justify-center items-center w-full">
-          <div className="h-28 p-2 w-full bg-white bg-opacity-85 rounded-3xl flex items-center justify-center overflow-hidden">
-            <div className={NunitoFont.className}>{comment}</div>
+            </div>
+            {/* コメント表示エリア */}
+            <div
+              className="flex justify-center items-center"
+              style={{
+                marginBottom: "calc(100vh - 88vh)",
+              }}
+            >
+              <div
+                className="p-2 bg-white bg-opacity-85 rounded-3xl flex items-center justify-center overflow-hidden"
+                style={{
+                  width: "calc(100vw - 80vw)",
+                  height: "calc(100vh - 89vh)",
+                }}
+              >
+                <div className={NunitoFont.className}>{comment}</div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        {/* 人物画像 */}
-        <div
-          className="z-30 flex justify-center items-center"
-          style={{
-            marginTop: "calc(100vh - 73vh)",
-            marginRight: "calc(100vw - 30vw)",
-          }}
-        >
-          <div className="relative w-32 h-32 overflow-hidden rounded-full">
-            <Image
-              alt="human"
-              src={humanImage}
-              layout="fill"
-              objectFit="contain"
-              className="rounded-full"
-            />
-          </div>
-        </div>
+      {/* Sidebar */}
+      <div className="w-1/6 h-full">
+        <Sidebar onSelectChange={handleSelectChange} />
       </div>
     </div>
   );
